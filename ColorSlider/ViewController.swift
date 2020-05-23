1c9277b216d978b7b8f813f5274755f62c61094b
Uemlianin Stepan <iosdevoloper@gmail.com>
2020-05-23 09:15:47 +0200

    Initial Commit

---
+++ ColorSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorSlider
+//
+//  Created by Stepan Uemlianin on 23/05/2020.
+//  Copyright © 2020 Stepan Uemlianin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
