525550e03764fec9bca72c8fac3835a9a518e1fe
Uemlianin Stepan <iosdevoloper@gmail.com>
2020-05-24 16:35:04 +0200

    Functional update

--- ColorSlider/ViewController.swift
+++ ColorSlider/ViewController.swift
@@ -16,31 +16,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenColorLabel: UILabel!
     @IBOutlet weak var blueColorLabel: UILabel!
     
-    @IBOutlet weak var redSliderLabel: UISlider!
-    @IBOutlet weak var greenSliderLabel: UISlider!
-    @IBOutlet weak var blueSliderLabel: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    let color = UIColor()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Label
-        redColorLabel.text = String(redSliderLabel.value)
-        greenColorLabel.text = String(greenSliderLabel.value)
-        blueColorLabel.text = String(blueSliderLabel.value)
+        redColorLabel.text = String(redSlider.value)
+        greenColorLabel.text = String(greenSlider.value)
+        blueColorLabel.text = String(blueSlider.value)
+        
+        //Slider
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
         
 
     }
 
     @IBAction func redSliderAction() {
-        redColorLabel.text = String(round(100 * redSliderLabel.value) / 100)
+        redColorLabel.text = String(round(100 * redSlider.value) / 100)
+        colorView.backgroundColor = UIColor.init(red: CGFloat(redSlider.value),
+                                                 green: CGFloat(greenSlider.value),
+                                                 blue: CGFloat(blueSlider.value),
+                                                 alpha: 1.0)
     }
     
     @IBAction func greenSliderAction() {
-        greenColorLabel.text = String(round(100 * greenSliderLabel.value) / 100)
+        greenColorLabel.text = String(round(100 * greenSlider.value) / 100)
+        colorView.backgroundColor = UIColor.init(red: CGFloat(redSlider.value),
+                                                 green: CGFloat(greenSlider.value),
+                                                 blue:CGFloat(blueSlider.value),
+                                                 alpha: 1.0)
     }
     
     @IBAction func blueSliderAction() {
-        blueColorLabel.text = String(round(100 * blueSliderLabel.value) / 100)
+        blueColorLabel.text = String(round(100 * blueSlider.value) / 100)
+        colorView.backgroundColor = UIColor.init(red: CGFloat(redSlider.value),
+                                                 green: CGFloat(greenSlider.value),
+                                                 blue:CGFloat(blueSlider.value),
+                                                 alpha: 1.0)
     }
 }
+
 
