5a04bdf042f7d2f7934f160e191930e0351b84ea
Uemlianin Stepan <iosdevoloper@gmail.com>
2020-05-24 15:30:58 +0200

    Fix interface part 5

--- ColorSlider/ViewController.swift
+++ ColorSlider/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redColorLabel: UILabel!
+    @IBOutlet weak var greenColorLabel: UILabel!
+    @IBOutlet weak var blueColorLabel: UILabel!
+    
+    @IBOutlet weak var redSliderLabel: UISlider!
+    @IBOutlet weak var greenSliderLabel: UISlider!
+    @IBOutlet weak var blueSliderLabel: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Label
+        redColorLabel.text = String(redSliderLabel.value)
+        greenColorLabel.text = String(greenSliderLabel.value)
+        blueColorLabel.text = String(blueSliderLabel.value)
+        
+
     }
 
-
+    @IBAction func redSliderAction() {
+        redColorLabel.text = String(round(100 * redSliderLabel.value) / 100)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenColorLabel.text = String(round(100 * greenSliderLabel.value) / 100)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueColorLabel.text = String(round(100 * blueSliderLabel.value) / 100)
+    }
 }
 
